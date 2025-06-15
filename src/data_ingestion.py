6703b9fb503b0bf2f566f5d9da740f6b130703b1
Rishi <raj22rishi@gmail.com>
2025-06-15 11:49:32 +0530

    s3 storage added as remote

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import logging
 import os
+import yaml
 
 log_dir = 'logs'
 os.makedirs(log_dir , exist_ok= True)
@@ -38,6 +39,23 @@ def load_data(data_url: str) -> pd.DataFrame:
     except Exception as e:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
+    
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """"Preprocess the data."""
@@ -68,7 +86,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path:str) 
 
 def main():
     try:
-        test_size = 0.2
+        # test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        
         data_path = 'https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url= data_path)
         final_df = preprocess_data(df)
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -40,6 +40,23 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
 
 def apply_tfidf(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
     """Apply TfIdf to the data."""
@@ -81,7 +98,9 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
 def main():
     try:
         
-        max_features = 50
+        # max_features = 50
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
 
